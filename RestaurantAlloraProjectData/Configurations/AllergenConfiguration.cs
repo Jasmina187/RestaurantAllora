ff1e5ac148d429a5b53699cde464be4985c52e7c
Jasmina187 <jasmina.ryustem@gmail.com>
2026-03-11 14:55:24 +0200

    Update ids in AllergenConfiguration

--- RestaurantAlloraProjectData/Configurations/AllergenConfiguration.cs
+++ RestaurantAlloraProjectData/Configurations/AllergenConfiguration.cs
@@ -14,78 +14,76 @@ namespace RestaurantAlloraProjectData.Configurations
         public void Configure(EntityTypeBuilder<Allergen> builder)
         {
             builder.HasData(
-                new Allergen
-                {
-                    AllergenId = Guid.Parse("a1f1c2d3-1111-4b11-8111-111111111111"),
-                    AllergenName = "Зърнени култури, съдържащи глутен"
-                },
             new Allergen
             {
-                AllergenId = Guid.Parse("b2f2c3d4-2222-4c22-8222-222222222222"),
+                AllergenId = Guid.Parse("dc08b4ec-5095-4811-a672-192301357e16"),
+                AllergenName = "Зърнени култури, съдържащи глутен"
+            },
+            new Allergen
+            {
+                AllergenId = Guid.Parse("42ccb79f-216b-4aab-9655-944d4f7b9823"),
                 AllergenName = "Ракообразни и продукти от тях"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("c3f3c4d5-3333-4d33-8333-333333333333"),
+                AllergenId = Guid.Parse("c4399ef7-4776-4b45-92fb-35ae3dd3f977"),
                 AllergenName = "Яйца и продукти от тях"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("d4f4c5d6-4444-4e44-8444-444444444444"),
+                AllergenId = Guid.Parse("c8f84b5a-4cd4-405b-adb4-2eeb69942cfc"),
                 AllergenName = "Риба и рибни продукти"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("e5f5c6d7-5555-4f55-8555-555555555555"),
+                AllergenId = Guid.Parse("d72de7c5-8e83-4d4c-a8e2-c5cdcf8da39e"),
                 AllergenName = "Фъстъци и продукти от тях"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("f6f6c7d8-6666-4066-8666-666666666666"),
+                AllergenId = Guid.Parse("4cab969c-d612-4a00-9d29-3aedc8d90f3b"),
                 AllergenName = "Соя и соеви продукти"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("a7a7c8d9-7777-4177-8777-777777777777"),
+                AllergenId = Guid.Parse("be7c8688-70de-4742-a524-d3d8e99616d8"),
                 AllergenName = "Мляко и млечни продукти"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("b8b8c9e1-8888-4288-8888-888888888888"),
+                AllergenId = Guid.Parse("32aabc2d-ef09-4d9f-b85f-973062edf531"),
                 AllergenName = "Ядки – бадеми, орехи, лешници, кашу и др."
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("c9c9d1e2-9999-4399-8999-999999999999"),
+                AllergenId = Guid.Parse("859bc440-5fd3-4a13-b827-48a2540ffc6c"),
                 AllergenName = "Целина и продукти от нея"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("d1d1e2f3-aaaa-44aa-8aaa-aaaaaaaaaaaa"),
+                AllergenId = Guid.Parse("2138d67d-b112-4d75-8340-4130c6d49f6f"),
                 AllergenName = "Синап и продукти от него"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("e2e2f3a4-bbbb-45bb-8bbb-bbbbbbbbbbbb"),
+                AllergenId = Guid.Parse("4eed56a2-0d6e-4c82-811f-d84066553249"),
                 AllergenName = "Сусамово семе и продукти от него"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("f3f3a4b5-cccc-46cc-8ccc-cccccccccccc"),
+                AllergenId = Guid.Parse("8459d499-ec62-49a3-b93c-fdd2a75c940b"),
                 AllergenName = "Лупина и продукти от нея"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("a4a4b5c6-dddd-47dd-8ddd-dddddddddddd"),
+                AllergenId = Guid.Parse("063e346a-e671-4710-835b-8f6132ee7744"),
                 AllergenName = "Серен диоксид и сулфиди"
             },
             new Allergen
             {
-                AllergenId = Guid.Parse("b5b5c6d7-eeee-48ee-8eee-eeeeeeeeeeee"),
+                AllergenId = Guid.Parse("945ea094-2772-4113-80f9-4b9c8fc5188d"),
                 AllergenName = "Мекотели и продукти от тях"
-            }
-        );
-
+            });
         }
     }
 }
